900f8b01cbb86ab2e863d8ce9e7c4c98cacad023
maximakymenko <maks.akymenko@gmail.com>
2019-07-25 19:27:19 +0200

    add new components exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Bio } from './bio';
 export { default as Layout } from './layout';
 export { default as Menu } from './menu';
 export { default as SEO } from './seo';
+export { default as Toggle } from './toggle';
+export { default as BuyMeACoffee } from './buymeacoffee'
