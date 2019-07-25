2ab74e7ee848f57c533ea02dba80aaeef6c084a2
maximakymenko <maks.akymenko@gmail.com>
2019-07-25 19:25:36 +0200

    create utils and shared folders

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,11 +1,31 @@
 import { createGlobalStyle } from 'styled-components'
 
-export const theme = {
+export const darkTheme = {
+  type: 'dark',
   primaryDark: '#0C0F13',
   primaryLight: '#C1C1C1',
   primaryGrey: '#8A8787',
   primaryYellow: '#FFCD42',
   primaryYellowHover: '#FFE59C',
+  // toggle colors
+  lightBlue: '#83D8FF',
+  blue: '#749DD6',
+  darkYellow: '#E8CDA5',
+  white: '#FFF'
+};
+
+export const lightTheme = {
+  type: 'light',
+  primaryDark: '#FFF',
+  primaryLight: '#0C0F13',
+  primaryGrey: '#8A8787',
+  primaryYellow: '#DCAA21',
+  primaryYellowHover: '#C7AB5A',
+  // toggle colors
+  lightBlue: '#83D8FF',
+  blue: '#749DD6',
+  darkYellow: '#E8CDA5',
+  white: '#FFF',
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -23,6 +43,8 @@ export const GlobalStyles = createGlobalStyle`
 
     background: ${({ theme }) => theme.primaryDark};
     color: ${({ theme }) => theme.primaryLight};
+
+    transition: background 0.3s ease-in, color 0.3s ease-in;
   }
 
   a {
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export const isMainPath = location => (
+  location.pathname === `${__PATH_PREFIX__}/` ||
+  location.pathname === `${__PATH_PREFIX__}/about` ||
+  location.pathname === `${__PATH_PREFIX__}/contact`
+)
